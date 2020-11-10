cb89b5b3e7d27bcfd648417fcaee2f713a4e52fb
sanket2498 <sanketdulange24@gmail.com>
2020-11-11 00:24:41 +0530

    Modified : Using Switch Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,26 +1,27 @@
 public class EmployeeWageComputation {
+	public static final int IS_FULL_TIME=1;
+        public static final int IS_PART_TIME=2;
+        public static final int WAGE_PER_HR=20;
+
 	public static void main(String[] args){
 		System.out.println("This is Employee Wage Computation problem");
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
-		int WAGE_PER_HR=20;
 		int empHr=0;
 		int empWage=0;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME){
-			System.out.println("Employee is Present");
-			empHr=8;
-			}
-		else if (empCheck == IS_PART_TIME){
-			System.out.println("Employee present part-time");
-			empHr=4;
-			}
-		else{
-			System.out.println("Employee is Absent");
-			empHr=0;
-		     }
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+			case IS_FULL_TIME:
+				System.out.println("Employee is Present");
+				empHr=8;
+				break;
+			case IS_PART_TIME:
+				System.out.println("Employee present part-time");
+				empHr=4;
+				break;
+			default:
+				System.out.println("Employee is Absent");
+				empHr=0;
+	     	}
 		empWage=empHr*WAGE_PER_HR;
 		System.out.println("Wage is: "+empWage);
 	}
 }
-
