854fc511b2f181d457478d4fa82d12a5b24cffe4
sanket2498 <sanketdulange24@gmail.com>
2020-11-11 00:28:06 +0530

    Added : Monthly Wages

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,11 +2,14 @@ public class EmployeeWageComputation {
 	public static final int IS_FULL_TIME=1;
         public static final int IS_PART_TIME=2;
         public static final int WAGE_PER_HR=20;
+	public static final int MAX_WORKING_DAYS=20;
 
 	public static void main(String[] args){
 		System.out.println("This is Employee Wage Computation problem");
 		int empHr=0;
 		int empWage=0;
+		int totalWage=0;
+		for (int days=0; days<MAX_WORKING_DAYS; days++) {
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
 			case IS_FULL_TIME:
@@ -22,6 +25,9 @@ public class EmployeeWageComputation {
 				empHr=0;
 	     	}
 		empWage=empHr*WAGE_PER_HR;
+		totalWage += empWage;
 		System.out.println("Wage is: "+empWage);
+		}
+		System.out.println("Total Wage is: "+totalWage);
 	}
 }
