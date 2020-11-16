d91b6bfe3fd9f1b523dc4021ad7bd35893f62575
sanket2498 <sanketdulange24@gmail.com>
2020-11-16 21:29:56 +0530

    Added : Code to manage wages of multiple companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
 public class EmployeeWageComputation {
-	public static final int IS_FULL_TIME=1;
-        public static final int IS_PART_TIME=2;
-	private String company;
-	private int wagePerHour;
-	private int workingDays;
-	private int workingHours;
-	private int totalEmpWage;
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
 
-	public EmployeeWageComputation(String company ,int wagePerHour,int workingDays,int workingHours){
-		this.company=company;
-		this.wagePerHour=wagePerHour;
-		this.workingDays=workingDays;
-		this.workingHours=workingHours;
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
+
+	public EmployeeWageComputation() {
+		companyEmpWageArray = new CompanyEmpWage[5];
 	}
 
+	private void addCompanyEmpWage(String company ,int wagePerHour,int workingDays,int workingHours){
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company,wagePerHour,workingDays,workingHours);
+		numOfCompany++;
+	}
+
+	private void computeEmpWage() {
+		for (int i =0; i < numOfCompany; i++) {
+			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
 	//Calculating monthly wage of employee
-	public void employeeMonthlyWage(){
+	private int computeEmpWage(CompanyEmpWage companyEmpWage){
 		//variables
-		int empHours=0;
-		int totalWorkingDays=0, totalEmpHours=0;
-		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
-		while (totalEmpHours <= workingHours && totalWorkingDays < workingDays){
+		int empHours=0, totalWorkingDays=0, totalEmpHours=0;
+		while (totalEmpHours <= companyEmpWage.workingHours && totalWorkingDays < companyEmpWage.workingDays){
                         totalWorkingDays++;
+			int empCheck=(int) Math.floor(Math.random() * 10) % 3;
 			switch(empCheck){
 				case IS_PART_TIME:
 					empHours=8;
@@ -33,26 +38,17 @@ public class EmployeeWageComputation {
 				default:
 					empHours=0;
 			}
-		totalEmpHours += empHours;
+			totalEmpHours += empHours;
 		}
-	System.out.println("total employee working hour - "+totalEmpHours);
-        System.out.println("total employee working days - "+totalWorkingDays);
-        totalEmpWage = totalEmpHours * wagePerHour;
+		System.out.println("total employee working hour - "+totalEmpHours);
+        	System.out.println("total employee working days - "+totalWorkingDays);
+        	return totalEmpHours * companyEmpWage.wagePerHour;
 	}
-
-	public String toString(){
-        	return "Total employee wage for company : "+company+" is - "+totalEmpWage;
-	}
-
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
-		//take user input for each company
-		EmployeeWageComputation tata = new EmployeeWageComputation("Tata",20,20,100);
-		EmployeeWageComputation reliance = new EmployeeWageComputation("Jio",10,25,150);
-		tata.employeeMonthlyWage();
-		System.out.println(tata);
-		reliance.employeeMonthlyWage();
-		System.out.println(reliance);
+		EmployeeWageComputation empWage = new EmployeeWageComputation();
+		empWage.addCompanyEmpWage("Tata",20,20,100);
+		empWage.addCompanyEmpWage("Jio",10,25,150);
+		empWage.computeEmpWage();
 	}
 }
-
